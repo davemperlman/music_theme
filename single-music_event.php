5a666444d19c6e45772c7674eb846c491f35d1e1
dave <daveperlman89@me.com>
2017-02-27 07:48:32 -0700

    Set music event featured image to top of page

--- single-music_event.php
+++ single-music_event.php
@@ -5,6 +5,7 @@ get_header();
 <section class="music-event">
 	<?php if(have_posts()): ?>
 	    <?php while(have_posts()): the_post(); ?>
+	    	<section class="music-event-image"><?php the_post_thumbnail(); ?></section> 
 	       	<h2><?php the_title(); ?></h2> 
 	       	<section class="music-event-content">
 		       <ul class="meta">
@@ -22,7 +23,7 @@ get_header();
 		       		<?php the_tags(); ?>
 		       </section>
 		  	</section>
-		  	<section class="music-event-image"><?php the_post_thumbnail(); ?></section> 
+		  	
 	    <?php endwhile;?>
 	<?php endif;?>
 </section>
