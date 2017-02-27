47ff2b27227bafe02fbdc207ebafd3471bf43cfb
dave <daveperlman89@me.com>
2017-02-27 07:39:17 -0700

    Small changes to home page, number of events displayed

--- events_loop.php
+++ events_loop.php
@@ -1,7 +1,7 @@
 <?php
 $loop = new WP_Query( array(
 	'post_type'      => 'music_event',
-	'posts_per_page' => '5',
+	'posts_per_page' => '15',
 	'category_name'  => 'shows',
 	'meta_query'     => array(
 		array(
@@ -23,7 +23,7 @@ if($loop->have_posts()) :
 		$loop->the_post(); ?>
 	<table class="event">
 		<tr>
-			<td><?php echo date('dS F', strtotime(get_post_meta(get_the_ID(), 'date')[0])); ?></td>
+			<td><?php echo date('jS F', strtotime(get_post_meta(get_the_ID(), 'date')[0])); ?></td>
 			<td><?php the_title(); ?></td>
 			<td><?php echo get_post_meta(get_the_ID(), 'location')[0]; ?></td>
 		</tr>
