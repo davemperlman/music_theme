331847b9f465bf351891743b726816a16001a017
dave <daveperlman89@me.com>
2017-03-04 09:44:29 -0700

    added loop to about.php and analytics script to header.php

--- page-about.php
+++ page-about.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 
-<section id="about">
-	<h2>About Dave Perlman</h2>
-	<p>
-		I am a musician and live performer/entertainer from Maine. Versatility in accommodations is a point of pride for me in my business. As a multi-instrumentalist with a diverse background in musical performance, I strive to deliver whatever a project demands to the best of my ability.
-	</p><br>
-	<p>
-		If you haven't heard me yet, go ahead and view some of the media posted on this site. Any questions can be directed to my <a href="http://108.176.232.90/wordpress/?page_id=71">contact</a> page.
-	</p>
-	<img src="http://www.entertainmentmaine.com/wp-content/uploads/2016/11/DSC03508.jpg" alt="me playing guitar">
-</section>
+ <section class="content">
+      <?php if(have_posts()): ?>
+         <?php while(have_posts()): the_post(); ?>
+            <section class="music-event-image">
+               <?php the_post_thumbnail(); ?>
+            </section>
+               <article class="blog-post">
+                  <section class="description">
+                     <p><?php the_content(); ?></p>
+                  </section>
+               </article>
+         <?php endwhile;?>
+      <?php endif;?>
+   </section>
 	
 
 <?php get_footer(); ?>
